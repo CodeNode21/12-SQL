46bc591f82b583a09c787fa2514b81171ba048a2
CodeNode21 <mbednara@gmail.com>
2019-07-22 07:59:11 -0400

    Set up Database with mock data

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+-- CREATE TABLE products (
+-- 	item_id INT NOT NULL AUTO_INCREMENT,
+--     product_name VARCHAR (225) NOT NULL,
+--     department_name VARCHAR (225) NOT NULL,
+--     price INT NOT NULL,
+--     stock_quantity INT NOT NULL,
+-- );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("High Density Foam Roller", "Fitness", 10, 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Dumbbells", "Fitness", 5, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Window Curtain", "Decor", 25, 61);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Bluetooth Headset", "Electronics", 150, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Printer", "Office", 99, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Moleskin Journal", "Office", 15, 150);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("3-D printer", "Hobbies", 350, 1);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ();
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ();
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ();
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ();
